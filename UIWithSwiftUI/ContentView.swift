0898537bcae8043a7884679e4cf6946a50959a41
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-10-27 18:58:14 +0200

    Initial Commit

---
+++ UIWithSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UIWithSwiftUI
+//
+//  Created by Alaa on 27/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIWithSwiftUI/UIWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIWithSwiftUIApp.swift
+//  UIWithSwiftUI
+//
+//  Created by Alaa on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct UIWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
